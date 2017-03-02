77c4a7a5af7a63d2320c202a43bcdedff38f38ef
Rafael Beraldo <rberaldo@cabaladada.org>
2017-03-01 23:38:34 -0300

    Limpa script de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,2 @@
 #!/usr/bin/env bash
-rsync -vzcrSLhp --exclude="deploy.sh" --chown=rberaldo:www-data ./_site/ pecorino:cabaladada.org
-
-# The options:
-# v - verbose
-# z - compress data
-# c - checksum, use checksum to find file differences
-# r - recursive
-# S - handle sparse files efficiently
-# L - follow links to copy actual files
-# h - show numbers in human-readable format
-# p - keep local file permissions (not necessarily recommended)
-# --exclude - Exclude files from being uploaded
+rsync -vzcrSLhp --exclude="deploy.sh" ./_site/ pecorino:cabaladada.org
